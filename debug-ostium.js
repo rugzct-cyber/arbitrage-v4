9f08fb278d90d7a48a69fbb7adedd07f16574cf7
rugzct-cyber <rugzct@gmail.com>
2025-12-06 04:39:53 +0100

    chore: Remove debug and test files

--- debug-ostium.js
+++
@@ -1,8 +0,0 @@
-const { httpGet } = require('./src/utils/fetcher');
-
-async function debug() {
-    const data = await httpGet("https://metadata-backend.ostium.io/PricePublish/latest-prices");
-    console.log("Raw item 0:", JSON.stringify(data[0], null, 2));
-}
-
-debug();
